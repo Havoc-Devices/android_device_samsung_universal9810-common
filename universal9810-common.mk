3a4dcc51bd392e0d94b2016b6cf88e5aeb3f081d
Andreas Schneider <asn@cryptomilk.org>
2020-09-07 17:08:22 +0200

    universal9810: permissions: give cameraserver access to more sensors
    This fixes:
    
    ActivityManager: No such permission: com.samsung.permission.HRM_EXT
    ActivityManager: android.content.pm.PackageManager$NameNotFoundException: com.samsung.permission.HRM_EXT
    ActivityManager: at android.app.ApplicationPackageManager.getPermissionInfo(ApplicationPackageManager.java:327)
    ActivityManager: at com.android.server.am.ActivityManagerService$PermissionController.isRuntimePermission(ActivityManagerService.java:5802)
    ActivityManager: at android.os.IPermissionController$Stub.onTransact(IPermissionController.java:153)
    ActivityManager: at android.os.Binder.execTransactInternal(Binder.java:1021)
    ActivityManager: at android.os.Binder.execTransact(Binder.java:994)
    ActivityManager: at com.android.server.SystemServer.startSensorService(Native Method)
    ActivityManager: at com.android.server.SystemServer.lambda$startBootstrapServices$0(SystemServer.java:832)
    ActivityManager: at com.android.server.-$$Lambda$SystemServer$UyrPns7R814g-ZEylCbDKhe8It4.run(Unknown Source:0)
    ActivityManager: at com.android.server.SystemServerInitThreadPool.lambda$submit$0$SystemServerInitThreadPool(SystemServerInitThreadPool.java:72)
    ActivityManager: at com.android.server.-$$Lambda$SystemServerInitThreadPool$jLyL3DFmbjsFesU5SGktD3NoWSc.run(Unknown Source:6)
    ActivityManager: at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:462)
    ActivityManager: at java.util.concurrent.FutureTask.run(FutureTask.java:266)
    ActivityManager: at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1167)
    ActivityManager: at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:641)
    ActivityManager: at com.android.internal.util.ConcurrentUtils$1$1.run(ConcurrentUtils.java:62)
    
    Change-Id: I5c57752d868c54fa3e0e00be87c5852389b2ded4

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -18,6 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/platform-samsung.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/platform-samsung.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.ethernet.xml \
